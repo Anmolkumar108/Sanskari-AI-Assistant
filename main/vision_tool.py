c4f10eedf67b300cfe3a9d62c4698d18587a22dd
Anmolkumar108 <akanmolsingh8@gmail.com>
2026-06-17 23:24:18 +0530

    tool

--- main/vision_tool.py
+++ main/vision_tool.py
@@ -5,3 +5,17 @@ from PIL import Image
 from google import genai
 from livekit.agents import function_tool
 
+load_dotenv()
+
+@function_tool
+async def analyze_screen() -> str:
+
+    try:
+
+        api_key = os.getenv("GOOGLE_API_KEY")
+
+        client = genai.Client(
+            api_key=api_key
+        )
+
+        
