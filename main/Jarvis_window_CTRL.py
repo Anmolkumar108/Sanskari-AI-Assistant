0b75dd47ae4ae2f783f4b91cafe367f62880c2b0
Anmolkumar108 <akanmolsingh8@gmail.com>
2026-06-26 12:03:19 +0530

    tool

--- main/Jarvis_window_CTRL.py
+++ main/Jarvis_window_CTRL.py
@@ -243,53 +243,6 @@
 
 
 
-# import os
-# import subprocess
-# import logging
-# import sys
-# import webbrowser
-# import difflib
-# import asyncio
-# import winreg
-
-# try:
-#     from livekit.agents import function_tool
-# except ImportError:
-#     def function_tool(func): 
-#         return func
-
-# try:
-#     import win32gui
-#     import win32con
-# except ImportError:
-#     win32gui = None
-#     win32con = None
-
-# try:
-#     import pygetwindow as gw
-# except ImportError:
-#     gw = None
-
-# # Setup
-# sys.stdout.reconfigure(encoding='utf-8')
-# logging.basicConfig(level=logging.INFO)
-# logger = logging.getLogger(__name__)
-
-# # 📂 Windows ke core profiles mapping list (From Code 1)
-# MAIN_USER_DIRS = [
-#     r"C:\Users\Anmol kumar\OneDrive\Desktop",
-#     r"C:\Users\Anmol kumar\Desktop",
-#     r"C:\Users\Anmol kumar\OneDrive\Documents",
-#     r"C:\Users\Anmol kumar\Documents",
-#     r"C:\Users\Anmol kumar\Downloads",
-#     r"C:\Users\Anmol kumar\Videos",
-#     r"C:\Users\Anmol kumar\Music",
-#     r"C:\Users\Anmol kumar" 
-# ]
-
-
-
-
 
 import os
 import subprocess
