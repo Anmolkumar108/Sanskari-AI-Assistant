eeaf18bf5bb1d94efedd17ac203ef48cf7b27928
Anmolkumar108 <akanmolsingh8@gmail.com>
2026-08-01 14:19:00 +0530

    Add jarvis ring animation

--- main/animations/jarvis_ring.py
+++ main/animations/jarvis_ring.py
@@ -35,4 +35,31 @@ class JarvisRing(QWidget):
         self.state = state
         self.update()
 
-    
+    def animate(self):
+
+        if self.state == "IDLE":
+            outer_speed = 1
+            inner_speed = -2
+            radar_speed = 1
+
+        elif self.state == "LISTENING":
+            outer_speed = 3
+            inner_speed = -4
+            radar_speed = 2
+
+        elif self.state == "THINKING":
+            outer_speed = 5
+            inner_speed = -6
+            radar_speed = 4
+
+        elif self.state == "SPEAKING":
+            outer_speed = 4
+            inner_speed = -5
+            radar_speed = 3
+
+        elif self.state == "ERROR":
+            outer_speed = 0
+            inner_speed = 0
+            radar_speed = 0
+
+        
