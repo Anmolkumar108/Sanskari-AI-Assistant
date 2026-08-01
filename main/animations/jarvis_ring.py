0fc498b7c3da80435114fb2b26941f7ff7fb2956
Anmolkumar108 <akanmolsingh8@gmail.com>
2026-08-01 14:18:37 +0530

    Add jarvis ring animation

--- main/animations/jarvis_ring.py
+++ main/animations/jarvis_ring.py
@@ -8,3 +8,31 @@ from PyQt6.QtGui import (
     QBrush,
     QPainterPath,
 )
+from PyQt6.QtWidgets import QWidget
+
+
+class JarvisRing(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.setMinimumSize(420, 420)
+
+        self.state = "IDLE"
+
+        self.outer_angle = 0
+        self.inner_angle = 0
+        self.radar_angle = 0
+
+        self.pulse = 0
+        self.pulse_direction = 1
+
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.animate)
+        self.timer.start(16)
+
+    def set_state(self, state):
+        self.state = state
+        self.update()
+
+    
