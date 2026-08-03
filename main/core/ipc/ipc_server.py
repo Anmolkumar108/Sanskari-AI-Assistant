d6aeff655682f76c7dcebc315eea346c540d8f1f
Anmolkumar108 <akanmolsingh8@gmail.com>
2026-08-03 11:53:48 +0530

    Add jarvis ring animation

--- main/core/ipc/ipc_server.py
+++ main/core/ipc/ipc_server.py
@@ -6,3 +6,21 @@ from main.core.event_bus import event_bus
 from main.core.ipc.messages import parse_message
 
 
+HOST = "127.0.0.1"
+PORT = 8765
+
+
+class IPCServer:
+
+    def __init__(self):
+
+        self.server = socket.socket(
+            socket.AF_INET,
+            socket.SOCK_STREAM
+        )
+
+        self.server.bind((HOST, PORT))
+
+        self.server.listen(1)
+
+    
